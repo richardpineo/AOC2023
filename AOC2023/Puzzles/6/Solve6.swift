73f16b4f607de7727bdafae6e1d65e8a198ae212
Richard Pineo <Richard.E.Pineo@ge.com>
2023-12-16 13:07:21 -0800

    6b

--- AOC2023/Puzzles/6/Solve6.swift
+++ AOC2023/Puzzles/6/Solve6.swift
@@ -8,30 +8,30 @@ class Solve6: PuzzleSolver {
 	}
 
 	func solveBExamples() -> Bool {
-		solveB("Example6") == 0
+		solveB(race: Self.exampleB) == 71503
 	}
 
 	var answerA = "4403592"
-	var answerB = "0"
-	
+	var answerB = "38017587"
+
 	struct Race {
 		var time: Int
 		var record: Int
-		
+
 		func distance(_ held: Int) -> Int {
 			let timeToMove = time - held
 			let velocity = held
 			return timeToMove * velocity
 		}
-		
-		var  beatsRecord: Int {
+
+		var numberBeatingRecord: Int {
 			(0 ... time).filter {
 				distance($0) > record
 			}
 			.count
 		}
 	}
-	
+
 	// Time:      7  15   30
 	// Distance:  9  40  200
 	private static let example: [Race] = [
@@ -39,6 +39,7 @@ class Solve6: PuzzleSolver {
 		.init(time: 15, record: 40),
 		.init(time: 30, record: 200),
 	]
+	private static let exampleB: Race = .init(time: 71530, record: 940_200)
 
 	// Time:        49     97     94     94
 	// Distance:   263   1532   1378   1851
@@ -48,23 +49,38 @@ class Solve6: PuzzleSolver {
 		.init(time: 94, record: 1378),
 		.init(time: 94, record: 1851),
 	]
+	private static let actualB: Race = .init(time: 49_979_494, record: 263_153_213_781_851)
 
 	func solveA() -> String {
 		solveA(races: Self.actual).description
 	}
 
 	func solveB() -> String {
-		solveB("Input6").description
+		solveB(race: Self.actualB).description
 	}
 
 	func solveA(races: [Race]) -> Int {
 		let score = races.reduce(1) {
-			$0 * $1.beatsRecord
+			$0 * $1.numberBeatingRecord
 		}
 		return score
 	}
 
-	func solveB(_: String) -> Int {
-		return 0
+	func solveB(race: Race) -> Int {
+		var minVal = 0
+		for t in 0 ... race.time {
+			if race.distance(t) > race.record {
+				minVal = t
+				break
+			}
+		}
+		var maxVal = 0
+		for t in 0 ... race.time {
+			if race.distance(race.time - t) > race.record {
+				maxVal = race.time - t
+				break
+			}
+		}
+		return maxVal - minVal + 1
 	}
 }
