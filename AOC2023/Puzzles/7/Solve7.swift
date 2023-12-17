1eec745cf7421ab0e86700695a70d1e21c1bd7c5
Richard Pineo <Richard.E.Pineo@ge.com>
2023-12-16 16:25:37 -0800

    7a

--- AOC2023/Puzzles/7/Solve7.swift
+++ AOC2023/Puzzles/7/Solve7.swift
@@ -5,14 +5,14 @@ import RegexBuilder
 
 class Solve7: PuzzleSolver {
 	func solveAExamples() -> Bool {
-		solveA("Example7") == 0
+		solveA("Example7") == 6440
 	}
 
 	func solveBExamples() -> Bool {
-		solveB("Example7") == 0
+		solveB("Example7") == 5905
 	}
 
-	var answerA = "0"
+	var answerA = "251106089"
 	var answerB = "0"
 
 	func solveA() -> String {
@@ -25,7 +25,15 @@ class Solve7: PuzzleSolver {
 
 	func solveA(_ filename: String) -> Int {
 		let game = load(filename)
-		return 0
+		let sorted = game.sorted
+//		let descriptions = sorted.map { "\($0.rank): \($0.stringForm)" }
+//		print( descriptions.joined(separator: "\n"))
+		var totalWinnings = 0
+		for rank in 1 ... sorted.count {
+			let winnings = sorted[rank - 1].bid * rank
+			totalWinnings += winnings
+		}
+		return totalWinnings
 	}
 
 	func solveB(_: String) -> Int {
@@ -34,11 +42,69 @@ class Solve7: PuzzleSolver {
 
 	struct Game {
 		struct Hand {
+			var stringForm: String
 			var cards: [Int]
 			var bid: Int
+
+			var rawValue: Int {
+				cards[0] * 10000 * 10000 +
+					cards[1] * 1000 * 1000 +
+					cards[2] * 100 * 100 +
+					cards[3] * 10 * 10 +
+					cards[4]
+			}
+
+			enum Rank: Int {
+				case fiveOfAKind = 6
+				case fourOfAKind = 5
+				case fullHouse = 4
+				case threeOfAKind = 3
+				case twoPair = 2
+				case onePair = 1
+				case highCard = 0
+			}
+
+			var rank: Rank {
+				let dict = cards.reduce([:]) { d, c -> [Int: Int] in
+					var d = d
+					let i = d[c] ?? 0
+					d[c] = i + 1
+					return d
+				}
+				let sorted = Array(dict.values.sorted().reversed())
+				switch sorted.count {
+				case 1:
+					return .fiveOfAKind
+				case 2:
+					if sorted[0] == 4 {
+						return .fourOfAKind
+					}
+					return .fullHouse
+				case 3:
+					if sorted[0] == 3 {
+						return .threeOfAKind
+					}
+					return .twoPair
+				case 4:
+					return .onePair
+				case 5:
+					return .highCard
+				default:
+					return .highCard
+				}
+			}
 		}
 
 		var hands: [Hand]
+
+		var sorted: [Hand] {
+			hands.sorted {
+				if $0.rank != $1.rank {
+					return $0.rank.rawValue < $1.rank.rawValue
+				}
+				return $0.rawValue < $1.rawValue
+			}
+		}
 	}
 
 	func convert(_ s: String) -> Int {
@@ -73,14 +139,17 @@ class Solve7: PuzzleSolver {
 
 		let hands: [Game.Hand] = lines.map {
 			let handMatch = $0.firstMatch(of: handEx)!
-			return .init(cards: [
-				convert(String(handMatch.output.1)),
-				convert(String(handMatch.output.2)),
-				convert(String(handMatch.output.3)),
-				convert(String(handMatch.output.4)),
-				convert(String(handMatch.output.5)),
-			],
-			bid: Int(String(handMatch.output.6))!)
+			return .init(
+				stringForm: $0,
+				cards: [
+					convert(String(handMatch.output.1)),
+					convert(String(handMatch.output.2)),
+					convert(String(handMatch.output.3)),
+					convert(String(handMatch.output.4)),
+					convert(String(handMatch.output.5)),
+				],
+				bid: Int(String(handMatch.output.6))!
+			)
 		}
 		return .init(hands: hands)
 	}
