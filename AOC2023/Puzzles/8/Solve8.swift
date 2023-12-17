9697efc763e74bcc5fe60ecbe8f329e6dbc1e587
Richard Pineo <Richard.E.Pineo@ge.com>
2023-12-16 20:33:20 -0800

    8b

--- AOC2023/Puzzles/8/Solve8.swift
+++ AOC2023/Puzzles/8/Solve8.swift
@@ -9,11 +9,11 @@ class Solve8: PuzzleSolver {
 	}
 
 	func solveBExamples() -> Bool {
-		solveB("Example8") == 0
+		solveB("Example8b") == 6
 	}
 
 	var answerA = "17287"
-	var answerB = "0"
+	var answerB = "18625484023687"
 
 	func solveA() -> String {
 		solveA("Input8").description
@@ -28,27 +28,30 @@ class Solve8: PuzzleSolver {
 		return network.walk()
 	}
 
-	func solveB(_: String) -> Int {
-		return 0
+	func solveB(_ filename: String) -> Int {
+		let network = load(filename)
+		return network.ghostWalk()
 	}
-	
+
 	struct Network {
 		enum Instruction {
 			case left
 			case right
 		}
+
 		var instructions: [Instruction]
-		
+
 		func instruction(for step: Int) -> Instruction {
 			return instructions[step % instructions.count]
 		}
-		
+
 		struct Node {
 			var left: String
 			var right: String
 		}
+
 		var nodes: [String: Node]
-		
+
 		func walk() -> Int {
 			var step = 0
 			var node = "AAA"
@@ -63,14 +66,62 @@ class Solve8: PuzzleSolver {
 			}
 			return step
 		}
+
+		var startingGhostNodes: [String] {
+			nodes.keys.filter { $0.last! == "A" }
+		}
+
+		func ghostExit(node: String) -> Bool {
+			node.last == "Z"
+		}
+
+		func takeStep(node: String, step: Int) -> String {
+			let found = nodes[node]!
+			if instruction(for: step) == .left {
+				return found.left
+			} else {
+				return found.right
+			}
+		}
+
+		struct Crumb: Hashable {
+			var node: String
+			var step: Int
+		}
+
+		func ghostWalkCycle(for startingNode: String) -> Int {
+			var crumbs: Set<Crumb> = .init()
+			crumbs.insert(.init(node: startingNode, step: 0))
+
+			var step = 1
+			var node = takeStep(node: startingNode, step: 0)
+			while !crumbs.contains(.init(node: node, step: step % instructions.count)) {
+				crumbs.insert(.init(node: node, step: step % instructions.count))
+
+				if ghostExit(node: node) {
+					return step
+				}
+				node = takeStep(node: node, step: step)
+				step = step + 1
+			}
+			return step
+		}
+
+		func ghostWalk() -> Int {
+			let exitCounts = startingGhostNodes.map { node in
+				ghostWalkCycle(for: node)
+			}
+			let steps = Int(MathHelper.lcm(of: exitCounts))
+			return steps
+		}
 	}
-	
+
 	func load(_ filename: String) -> Network {
 		let lines = FileHelper.load(filename)!.filter { !$0.isEmpty }
 		let instructions: [Network.Instruction] = lines[0].map {
 			$0 == "R" ? .right : .left
 		}
-		
+
 		// BBB = (DDD, EEE)
 		let nodeEx = Regex {
 			Capture { OneOrMore(.word) }
@@ -80,7 +131,7 @@ class Solve8: PuzzleSolver {
 			Capture { OneOrMore(.word) }
 			One(")")
 		}
-		
+
 		var nodes: [String: Network.Node] = [:]
 		lines.dropFirst().forEach {
 			let nodeMatch = $0.firstMatch(of: nodeEx)!
