21bfde7b113a7c7a6e062958ee28730e1acbbe9b
Richard Pineo <Richard.E.Pineo@ge.com>
2023-12-23 18:55:10 -0800

    15b

--- AOC2023/Puzzles/15/Solve15.swift
+++ AOC2023/Puzzles/15/Solve15.swift
@@ -8,11 +8,11 @@ class Solve15: PuzzleSolver {
 	}
 
 	func solveBExamples() -> Bool {
-		solveB("Example15") == 0
+		solveB("Example15") == 145
 	}
 
 	var answerA = "518107"
-	var answerB = "0"
+	var answerB = "303404"
 
 	func solveA() -> String {
 		solveA("Input15").description
@@ -40,7 +40,59 @@ class Solve15: PuzzleSolver {
 		return hashed.reduce(0, +)
 	}
 
-	func solveB(_: String) -> Int {
-		return 0
+	struct Lens {
+		var label: String
+		var focal: Int
+	}
+	
+	func solveB(_ filename: String) -> Int {
+		let raw = FileHelper.load(filename)!
+			.filter { !$0.isEmpty }
+			.first!
+		let commands = raw.split(separator: ",").map(String.init)
+
+		var boxes: [[Lens]] = .init(repeating: [], count: 256)
+		
+		commands.forEach { command in
+			if command.last! == "-" {
+				let label = command.subString(start: 0, count: command.count - 1)
+				let boxIndex = applyHash(label)
+				var box = boxes[boxIndex]
+				let found = box.firstIndex {
+					$0.label == label
+				}
+				if let found = found {
+					box.remove(at: found)
+				}
+				boxes[boxIndex] = box
+			} else {
+				let focal = Int(String(command.last!))!
+				let label = command.subString(start: 0, count: command.count - 2)
+				let boxIndex = applyHash(label)
+				var box = boxes[boxIndex]
+				let found = box.firstIndex {
+					$0.label == label
+				}
+				if let found = found {
+					box[found].focal = focal
+				} else {
+					box.append(.init(label: label, focal: focal))
+				}
+				boxes[boxIndex] = box
+			}
+		}
+		
+		var power = 0
+		boxes.enumerated().forEach { box in
+			box.element.enumerated().forEach { lens in
+				let lensPower =
+					(box.offset + 1) *
+				(lens.offset + 1) *
+				(lens.element.focal)
+				power = power + lensPower
+			}
+		}
+		
+		return power
 	}
 }
