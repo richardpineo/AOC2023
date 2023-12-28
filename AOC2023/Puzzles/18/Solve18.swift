102d60577487734fa5af936eea8ecd81acb7a92d
Richard Pineo <Richard.E.Pineo@ge.com>
2023-12-27 20:07:13 -0800

    18b

--- AOC2023/Puzzles/18/Solve18.swift
+++ AOC2023/Puzzles/18/Solve18.swift
@@ -8,11 +8,11 @@ class Solve18: PuzzleSolver {
 	}
 
 	func solveBExamples() -> Bool {
-		solveB("Example18") == 952408144115
+		solveB("Example18") == 952_408_144_115
 	}
 
 	var answerA = "49578"
-	var answerB = "0"
+	var answerB = "52885384955882"
 
 	func solveA() -> String {
 		solveA("Input18").description
@@ -21,173 +21,77 @@ class Solve18: PuzzleSolver {
 	func solveB() -> String {
 		solveB("Input18").description
 	}
-	
+
 	func solveA(_ filename: String) -> Int {
-		let steps = load(filename)
-		
-		// convert into positions
-		var current = Position2D.origin
-		var trench: [Position2D] = .init([current])
-		var minPos: Position2D = .origin
-		var maxPos: Position2D = .origin
-		steps.forEach {
-			for _ in 0 ..< $0.distance {
-				current = current.offset($0.heading, 1)
-				trench.append(current)
-				if current.x < minPos.x {
-					minPos.x = current.x
-				}
-				if current.x > maxPos.x {
-					maxPos.x = current.x
-				}
-				if current.y < minPos.y {
-					minPos.y = current.y
-				}
-				if current.y > maxPos.y {
-					maxPos.y = current.y
-				}
-			}
-		}
-		minPos = minPos.offset(-1,-1)
-		maxPos = maxPos.offset(1,1)
-
-		let loop: Set<Position2D> = .init(trench)
-		
-		var interior = 0
-		for x in minPos.x ... maxPos.x {
-			for y in minPos.y ... maxPos.y {
-				if pointInLoop(pos: .init(x,y), loop: loop, maxPos: maxPos) {
-					interior += 1
-				}
-			}
-		}
-		return interior + loop.count
+		solve(filename, isA: true)
 	}
 
-	func solveA2(_ filename: String) -> Int {
-		let steps = load(filename)
-		
-		// convert into positions
-		var current = Position2D.origin
-		var trench: [Position2D] = .init([current])
-		var minPos: Position2D = .origin
-		steps.forEach {
-			for _ in 0 ..< $0.distance {
-				current = current.offset($0.heading, 1)
-				trench.append(current)
-				if current.x < minPos.x {
-					minPos.x = current.x
-				}
-				if current.y < minPos.y {
-					minPos.y = current.y
-				}
-			}
-		}
-		
-		// offset everything
-		let trenchOffset: [Position2D] = trench.map {
-			.init($0.x - minPos.x, $0.y - minPos.y)
-		}
-		
-		var grid: Grid2D = .init(positions: trenchOffset, value: "#")
-		grid.allPositions.forEach {
-			if grid.value($0) != "#" {
-				grid.setValue( $0, ".")
-			}
-		}
-		print(grid.debugDisplay)
-		
-		let loop: Set<Position2D> = .init(trenchOffset)
-		
-		grid.allPositions.forEach {
-			let v = grid.value($0)
-			if v == "." {
-				if pointInLoop(pos: $0, loop: loop, maxPos: grid.maxPos) {
-					grid.setValue($0, "#")
-				}
-			}
-		}
-		print(grid.debugDisplay)
-		let count = grid.allPositions.reduce(0) {
-			if grid.value($1) == "#" {
-				return $0 + 1
-			}
-			return $0
-		}
-		return count
+	func solveB(_ filename: String) -> Int {
+		solve(filename, isA: false)
 	}
 
-	func solveB(_: String) -> Int {
-		return 0
+	func solve(_ filename: String, isA: Bool) -> Int {
+		let steps = load(filename, isA: isA)
+		return area(steps: steps)
 	}
-	
+
+	// Shoelace formula stolen from: https://github.com/mnvr/advent-of-code-2023/blob/main/18.swift
+	func area(steps: [Step]) -> Int {
+		var px = 0, py = 0, s = 0
+		for step in steps {
+			var x, y: Int
+			switch step.heading {
+			case .east: (x, y) = (px + step.distance, py)
+			case .west: (x, y) = (px - step.distance, py)
+			case .south: (x, y) = (px, py - step.distance)
+			case .north: (x, y) = (px, py + step.distance)
+			}
+			s += (py + y) * (px - x)
+			s += step.distance
+			(px, py) = (x, y)
+		}
+
+		return abs(s) / 2 + 1
+	}
+
 	struct Step {
 		var heading: Heading
 		var distance: Int
-		var color: String
 	}
-	
-	func pointInLoop(pos: Position2D, loop: Set<Position2D>, maxPos: Position2D) -> Bool {
-		if loop.contains(pos) {
-			return false
-		}
 
-		// ray-casting algorithm
-		var crossCountHorizontal = 0
-		
-		var wasOnLoopFromNorth: Bool?
-		
-		for x in pos.x ... maxPos.x {
-			let checkPoint: Position2D = .init(x, pos.y)
-			
-			var crossesLine = false
-			
-			let nowOnLoop = loop.contains(checkPoint)
-			if nowOnLoop {
-				if wasOnLoopFromNorth != nil {
-					// Was on the loop, nothing to do.
-				} else {
-					// Now on the loop - is it a horizontal
-					if loop.contains(checkPoint.offset(1,0)) {
-						// Did we come from the north
-						wasOnLoopFromNorth = loop.contains(checkPoint.offset(0, -1))
-					} else {
-						crossesLine = true
-					}
-				}
-			} else {
-				// No longer on loop
-				if let fromNorth = wasOnLoopFromNorth {
-					let wentSouth =	!loop.contains(checkPoint.offset(-1, 1))
-					if fromNorth != wentSouth {
-						crossesLine = true
-					}
-					wasOnLoopFromNorth = nil
-				}
-			}
-			crossCountHorizontal = crossCountHorizontal + (crossesLine ? 1 : 0)
-		
-		}
-		return crossCountHorizontal % 2 == 1
-	}
-	
-	func load(_ filename: String) -> [Step] {
+	func load(_ filename: String, isA: Bool) -> [Step] {
 		let lines = FileHelper.load(filename)!.filter { !$0.isEmpty }
 		return lines.map {
 			let tokens = $0.split(separator: " ")
-			let heading: Heading = switch tokens[0] {
-			case "R": .east
-			case "D":  .north
-			case "L":  .west
-			case "U":  .south
-			default:
-				 .west
+			if isA {
+				let heading: Heading = switch tokens[0] {
+				case "R": .east
+				case "D": .north
+				case "L": .west
+				case "U": .south
+				default:
+					.west
+				}
+				return .init(
+					heading: heading,
+					distance: Int(tokens[1])!
+				)
+			} else {
+				let hexDistance = String(tokens[2]).subString(start: 2, count: tokens[2].count - 4)
+				let headingStr = String(tokens[2]).subString(start: tokens[2].count - 2, count: 1)
+				let heading: Heading = switch headingStr {
+				case "0": .east
+				case "1": .north
+				case "2": .west
+				case "3": .south
+				default:
+					.west
+				}
+				return .init(
+					heading: heading,
+					distance: Int(hexDistance, radix: 16)!
+				)
 			}
-			return .init(
-				heading: heading,
-				distance: Int(tokens[1])!,
-				color: String(tokens[2])
-			)
 		}
 	}
 }
